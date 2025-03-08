8e2eddb76c3ec9ba77652e345625633b472c8d27
Akshay Jadhav <akshayjadhav2133@gmail.com>
2025-03-08 09:17:54 +0530

    added missing index file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+const PORT = 3000;
+
+const bascicRoute=require("./src/routes/basicRoutes");
+app.use('/',bascicRoute);
+
+const shortenRoute=require("./src/routes/shortenRoutes1");
+app.use('/shorten',shortenRoute);
+
+const redirtRoute=require("./src/routes/redirectRoutes");
+app.use('/redirect',redirtRoute);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}/`);
+});
