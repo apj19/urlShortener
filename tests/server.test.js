4b03d00a999a2cdf8292202443cc8725a20dd1eb
Akshay Jadhav <akshayjadhav2133@gmail.com>
2025-03-08 11:30:51 +0530

    basictest added

--- tests/server.test.js
+++ tests/server.test.js
@@ -16,4 +16,22 @@ describe("Check server health", () => {
       expect(response.status).toBe(200);
     //   expect(response.body).toHaveProperty("message", "Server is running");
     });
-  });
+});
+
+describe("Should return shortcode", () => {
+    test("Post /shorten should return shortcode", async () => {
+      const response = await request(app).post("/shorten").send({"longUrl":"https://www.netflix.com/in/"});
+      expect(response.status).toBe(200 || 201);
+    //   expect(response.body).toHaveProperty("message", "Server is running");
+    });
+});
+
+
+
+describe("Should redirect with 200 for netflix", () => {
+    test("GET /redirect?code={code} should return 200", async () => {
+      const response = await request(app).get("/redirect?code=testfb");
+      expect(response.status).toBe(302);
+    //   expect(response.body).toHaveProperty("message", "Server is running");
+    });
+});
